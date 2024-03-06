4752b6fa6b90812a9363f22bded61030b1f69f0a
aydcereal <jordyfigueroa93@icloud.com>
2024-03-05 22:17:04 -0800

    Add expenses.json and update AllExpenses screen

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,7 +1,56 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
+import expenses from "../data/expenses.json";
 
 function AllExpenses() {
-  return <View></View>;
+  console.log(expenses[0].title);
+  return (
+    <ScrollView>
+      {expenses.map((item, index) => {
+        return (
+          <View key={index} style={styles.container}>
+            <View>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.date}>{item.date}</Text>
+            </View>
+            <View style={styles.amountContainer}>
+              <Text style={styles.amount}>{item.amount}</Text>
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 25,
+    marginVertical: 5,
+    padding: 10,
+    flex: 1,
+    alignContent: "center",
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  date: {},
+  amountContainer: {
+    backgroundColor: "gray",
+    minWidth: 80,
+    padding: 15,
+    borderRadius: 5,
+  },
+  amount: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+  },
+});
 
 export default AllExpenses;
