aa5cc8eced127412808c58e9c2ddb24cffecb9b4
aydcereal <jordyfigueroa93@icloud.com>
2024-04-02 21:26:29 -0700

    Refactor ExpenseForm component to use useState hook for input values

--- components/manageExpenses/ExpenseForm.js
+++ components/manageExpenses/ExpenseForm.js
@@ -1,20 +1,44 @@
 import { View, StyleSheet, Text } from "react-native";
 import Input from "./Input";
+import { useState } from "react";
 
 function ExpenseForm() {
-  function amountChangedHandler() {}
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    title: "",
+  });
+
+  function inputChangedHandler(inputIdentifier, enteredValue) {
+    setInputValues((curInputValues) => {
+      return {
+        ...curInputValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
+    console.log("change");
+    console.log(inputIdentifier);
+  }
 
   return (
     <View style={styles.form}>
       <Text style={styles.title}>Your Expense</Text>
-      <Input label="Title" textInputConfig={{ multiLine: true }} />
+      <Input
+        label="Title"
+        textInputConfig={{
+          multiLine: true,
+          onChange: inputChangedHandler.bind(this, "title"),
+          value: inputValues.title,
+        }}
+      />
       <View style={styles.rowContainer}>
         <Input
           style={{ flex: 1 }}
           label="Amount"
           textInputConfig={{
             keyboardType: "decimal-pad",
-            onChange: amountChangedHandler,
+            onChange: inputChangedHandler.bind(this, "amount"),
+            value: inputValues.amount,
           }}
         />
         <Input
@@ -23,7 +47,8 @@ function ExpenseForm() {
           textInputConfig={{
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
-            onChange: () => {},
+            onChange: inputChangedHandler.bind(this, "date"),
+            value: inputValues.date,
           }}
         />
       </View>
